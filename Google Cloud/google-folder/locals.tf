e7b953fe1a6e4bd37d10ec9ff184ddbbbd68ef75
dgavrischuk <dgavrischuk@sciforce.solutions>
2022-07-13 03:30:49 +0300

    yaml converter added

--- Google Cloud/google-folder/locals.tf
+++ Google Cloud/google-folder/locals.tf
@@ -1,15 +1,67 @@
 locals {
-  level_3 = flatten([for key, data in var.folders_map_3 :
+  folders_yml = fileexists(var.folders_yml) ? file(var.folders_yml) : null
+
+  folders = local.folders_yml == null ? {} : yamldecode(local.folders_yml)
+# -----------------------------------------------------------------------
+#                         data readed from file
+# -----------------------------------------------------------------------
+  folders_level_1 = flatten([
+    for app, config in local.folders :
+    lookup(config, "level_1", null)
+  ])
+  folders_level_2 = flatten([
+    for app, config in local.folders :
+    lookup(config, "level_2", null)
+    if lookup(config, "level_2", null) != null
+  ])
+  folders_level_3 = flatten([
+    for app, config in local.folders :
+    lookup(config, "level_3", null)
+    if lookup(config, "level_3", null) != null
+  ])
+#------------------------------------------------------------------------- 
+#                     data converted to 
+#                     name = "name"
+#                     parent = "parent"
+#------------------------------------------------------------------------- 
+  level_1 = flatten([for folder in local.folders_level_1 : [
+    for value in folder["name"] :
+    {
+      parent = folder["parent"]
+      name   = value
+    }]
+  ])
+
+  level_2 = flatten([for folder in local.folders_level_2 : [
+    for value in folder["name"] :
+    {
+      parent = folder["parent"]
+      name   = value
+    }]
+  ])
+
+  level_3 = flatten([for folder in local.folders_level_3 : [
+    for value in folder["name"] :
+    {
+      parent = folder["parent"]
+      name   = value
+    }]
+  ])
+#-------------------------------------------------------------------------
+#                    data prepared to use in 
+#                    terraform resources
+#-------------------------------------------------------------------------
+  resource_level_3 = flatten([for key, data in local.level_3 :
     [for key, value in local.data_level_2 : {
       name   = data.name
       parent = value.id
       } if
-      value.parent_name == regex("^(\\w*)/", data.parent)[0] &&
-      value.name == regex("/(\\w*)$", data.parent)[0]
+      value.parent_name == split("/", data.parent)[0] &&
+      value.name == split("/", data.parent)[1]
     ]
   ])
   data_level_2 = flatten([
-    for key, value in local.level_2 : [
+    for key, value in local.resource_level_2 : [
       for key, data in google_folder.level_2[*] : [
         for folder, config in data : {
           name        = value.name
@@ -21,10 +73,10 @@ locals {
       ]
     ]
   ])
-  level_2 = [for key, data in var.folders_map_2 : {
-    name        = data.name
-    parent      = local.data_level_1["${data.parent}"]
-    parent_name = data.parent
+  resource_level_2 = [for key, value in local.level_2 : {
+    name        = value.name
+    parent      = local.data_level_1["${value.parent}"]
+    parent_name = value.parent
     }
   ]
 
--- Google Cloud/google-folder/outputs.tf
+++ Google Cloud/google-folder/outputs.tf
@@ -1,23 +1,27 @@
-# output "level_1_folders" {
-#   value = google_folder.level_1[*]
+# output "folders" {
+#   value = local.folders
 # }
 
-# output "level_2_folders" {
-#   value = google_folder.level_2[*]
+# output "level_1" {
+#   value = local.level_1
 # }
-
-output "data_level_2" {
-  value = local.data_level_2
-}
-
-output "level_3" {
-  value = local.level_3
-}
 
 # output "level_2" {
 #   value = local.level_2
 # }
 
+# output "level_3" {
+#   value = local.level_3
+# }
+
 output "level_1" {
-  value = local.data_level_1
+  value = local.level_1
+}
+
+output "level_2" {
+  value = local.level_2
+}
+
+output "level_3" {
+  value = local.level_3
 }
