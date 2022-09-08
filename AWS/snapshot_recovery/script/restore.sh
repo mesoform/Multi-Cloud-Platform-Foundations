ebedf8d7279e281bdca96f7c8fa265c78e370b3e
dgavrischuk <dgavrischuk@sciforce.solutions>
2022-09-08 22:10:17 +0300

    variables added

--- AWS/snapshot_recovery/script/restore.sh
+++ AWS/snapshot_recovery/script/restore.sh
@@ -3,24 +3,27 @@ set +x
 set -e
 
 MAX_COUNT=100
-REGION="eu-central-1"
-AZ="eu-central-1a"
 INSTANCEID=$1
 SNAPSHOTID=$2
+AZ=$3
+DEVICENAME="\"$4\""
 
-if [[ -z $INSTANCEID || -z $SNAPSHOTID ]]; then 
-  echo "To execute the script, you need to specify two parameters:"
-  echo "restore.sh <instance id> <snapshot id>"
-  echo "Please specify instance and snapshot ids"
+REGION=$(echo $AZ | sed 's/.$//')
+
+if [[ -z $INSTANCEID || -z $SNAPSHOTID || -z $AZ || -z $DEVICENAME ]]; then 
+  echo "To execute the script, you need to specify four parameters:"
+  echo "./restore.sh <instance id> <snapshot id> <availability-zone> <device name>"
+  echo "Please specify requirement parameters"
   exit 1
 fi
 
-NEWVOLUMEID=$(aws ec2 create-volume --size 8 --region $REGION --availability-zone $AZ --volume-type gp2 --snapshot-id $2 | jq '.VolumeId' | sed -e 's/^"//' -e 's/"$//')
+NEWVOLUMEID=$(aws ec2 create-volume --size 8 --region $REGION --availability-zone $AZ --volume-type gp2 --snapshot-id $SNAPSHOTID --tag-specifications 'ResourceType=volume, Tags=[{Key=name, Value=restored}]'| jq '.VolumeId' | sed -e 's/^"//' -e 's/"$//')
 echo "New volume from snapshot:" $NEWVOLUMEID
 
-OLDVOLUMEID=$(aws ec2 describe-instances --filters Name=instance-id,Values=$INSTANCEID | jq  ".Reservations[].Instances[].BlockDeviceMappings[] | {DeviceName: .DeviceName, VolumeId: .Ebs.VolumeId}" | jq '. | select(.DeviceName=="/dev/sdf") | .VolumeId' | sed -e 's/^"//' -e 's/"$//') 
+#OLDVOLUMEID=$(aws ec2 describe-instances --filters Name=instance-id,Values=$INSTANCEID | jq  ".Reservations[].Instances[].BlockDeviceMappings[] | {DeviceName: .DeviceName, VolumeId: .Ebs.VolumeId}" | jq '. | select(.DeviceName=="/dev/sdf") | .VolumeId' | sed -e 's/^"//' -e 's/"$//') 
+OLDVOLUMEID=$(aws ec2 describe-instances --filters Name=instance-id,Values=$INSTANCEID | jq  ".Reservations[].Instances[].BlockDeviceMappings[] | {DeviceName: .DeviceName, VolumeId: .Ebs.VolumeId}" | jq ". | select(.DeviceName==$DEVICENAME) | .VolumeId" | sed -e 's/^"//' -e 's/"$//') 
 if [[ -z $OLDVOLUMEID ]]; then 
-  echo "No device in /dev/sdf at instance "$INSTANCEID
+  echo "No device in "$DEVICENAME" at instance "$INSTANCEID
   exit 1
 fi
 echo "Old volume:" $OLDVOLUMEID
@@ -31,7 +34,7 @@ echo "Stopping instance "$INSTANCEID"..."
 CYCLES_COUNT=0
 while [[ $STATUS != "stopped" && $CYCLES_COUNT -lt $MAX_COUNT ]];
 do 
-    STATUS=$(aws ec2 describe-instances --filters Name=instance-id,Values=i-0cb513f9c3ce03653 | jq ".Reservations[].Instances[].State.Name" | sed -e 's/^"//' -e 's/"$//')
+    STATUS=$(aws ec2 describe-instances --filters Name=instance-id,Values=$INSTANCEID | jq ".Reservations[].Instances[].State.Name" | sed -e 's/^"//' -e 's/"$//')
     CYCLES_COUNT=$((CYCLES_COUNT+1))
 done
 
@@ -53,7 +56,7 @@ echo "Starting instance "$INSTANCEID"..."
 CYCLES_COUNT=0
 while [[ $STATUS != "running" && $CYCLES_COUNT -lt $MAX_COUNT ]];
 do 
-    STATUS=$(aws ec2 describe-instances --filters Name=instance-id,Values=i-0cb513f9c3ce03653 | jq ".Reservations[].Instances[].State.Name" | sed -e 's/^"//' -e 's/"$//')
+    STATUS=$(aws ec2 describe-instances --filters Name=instance-id,Values=$INSTANCEID | jq ".Reservations[].Instances[].State.Name" | sed -e 's/^"//' -e 's/"$//')
     CYCLES_COUNT=$((CYCLES_COUNT+1))
 done
 
