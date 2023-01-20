798e092109b9c90e48a9952f6224cf7321d65b0e
Alex Bailey <alex.bailey@mesoform.com>
2023-01-20 12:32:15 +0000

    GCP_FOLDER_ID environment variable for project deletion step

--- tests/gcp/deployment/google_project/delete_projects.sh
+++ tests/gcp/deployment/google_project/delete_projects.sh
@@ -7,11 +7,10 @@ echo $RESOURCES
 mapfile -t RESOURCE_NAMES< <(echo "$RESOURCES" | jq -r 'keys | .[] ')
 for name in "${RESOURCE_NAMES[@]}"; do
   PROJECT_ID=$(echo "$RESOURCES" | jq ".\"${name}\"" | jq -c ".project_id" | tr -d \")
-  SUFFIX="-<project_date>"
+  SUFFIX="-<project_date>" # ignore suffix when searching for project ids using gcloud
   echo $PROJECT_ID
   PROJECT_ID_PREFIX=${PROJECT_ID%"$SUFFIX"}
   echo "Deleting $PROJECT_ID_PREFIX projects"
-  echo "PROJECT_ID_PREFIX=$PROJECT_ID_PREFIX"
   for project in $(gcloud projects list --filter "parent.id:$GCP_FOLDER_ID AND projectId:${PROJECT_ID_PREFIX%-<project_date>}*" --format=json | jq -c ".[].projectId" | tr -d \"); do
     echo "Deleting project: $project"
     gcloud projects delete $project --quiet
