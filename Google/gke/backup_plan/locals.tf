53f37c5a79e54bc405afc184b4f91570c95265b5
Alex Bailey <alex.bailey@mesoform.com>
2023-01-24 16:15:01 +0000

    Fix typo and remove comments

--- Google/gke/backup_plan/locals.tf
+++ Google/gke/backup_plan/locals.tf
@@ -8,17 +8,6 @@ locals {
 
   backup_plans_specs_unmerged = var.backup_plans_specs == null ? local.backup_plans_components_specs : var.backup_plans_specs
 
-#  # Attempted workarounds which didn't work
-#  # This errors if var.backup_plans_specs is {} as the conditions are of different types.
-#  backup_plans_specs_unmerged = var.backup_plans_specs == null ? local.backup_plans_components_specs : var.backup_plans_specs
-#  # This would still say that var.backup_plans_specs is object with no attributes so type conflict
-#  backup_plans_specs_unmerged = var.backup_plans_specs == null || var.backup_plans_specs == {} ? local.backup_plans_components_specs : var.backup_plans_specs == {} ? null : var.backup_plans_specs
-#  # This would error in unit tests saying length(null) not allowed. Which is odd because you would think if the first value is true, the second one shouldn't be evaluated.
-#  backup_plans_specs_unmerged = var.backup_plans_specs == null || length(var.backup_plans_specs)<=0 ? local.backup_plans_components_specs : var.backup_plans_specs
-#  # These would error in deployment tests saying that local.backup_plans_specs contains keys not determined until deployment
-#  backup_plans_specs_unmerged = try(length(var.backup_plans_specs)<=0, true)? local.backup_plans_components_specs : var.backup_plans_specs
-#  backup_plans_specs_unmerged = try(var.backup_plans_specs == null ? local.backup_plans_components_specs : var.backup_plans_specs, var.backup_plans_specs)
-
   backup_plans_specs = { for backup_plan, config in local.backup_plans_specs_unmerged :
     backup_plan => merge(local.backup_plans_components_common == null ? {} : local.backup_plans_components_common, config)
   }
