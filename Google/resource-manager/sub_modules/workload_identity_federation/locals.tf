d62192158499426122c94623bb819e05cc7bf5cd
Alex Bailey <alex.bailey@mesoform.com>
2023-02-28 15:58:10 +0000

    Create default configurations for known OIDC issuers

--- Google/resource-manager/sub_modules/workload_identity_federation/locals.tf
+++ Google/resource-manager/sub_modules/workload_identity_federation/locals.tf
@@ -1,13 +1,6 @@
 locals  {
-  bitbucket_workspace = lookup({}, "", "")
-  known_issuers = {
-    github-actions = "https://token.actions.githubusercontent.com"
-    bitbucket-pipelines = "https://api.bitbucket.org/2.0/workspaces/%s/pipelines-config/identity/oidc"
-  }
-#  identity_pool_providers_merged = { for pool, specs in var.workload_identity_pool : pool => [
-#      for provider, provider_specs in lookup(specs, "providers", {}):
-#    ]
-#  }
+
+
   identity_pool_providers_transformed = concat([
     for pool, specs in var.workload_identity_pool : [
       for provider, provider_specs in lookup(specs, "providers", null): merge({
@@ -19,5 +12,40 @@ locals  {
   ]...)
 
 
-  identiy_pool_providers = zipmap([for provider in local.identity_pool_providers_transformed: "${provider.pool}_${provider.provider}"], local.identity_pool_providers_transformed )
+  identity_pool_providers_raw = zipmap([for provider in local.identity_pool_providers_transformed: "${provider.pool}_${provider.provider}"], local.identity_pool_providers_transformed )
+
+  identity_pool_providers_map_trusted = {
+    for provider, specs in local.identity_pool_providers_raw: provider =>
+      contains(keys(local.trusted_issuer_templates), try(specs.oidc.issuer, "null")) ? local.trusted_issuer_templates[specs.oidc.issuer]: null
+  }
+
+  identity_pool_providers = {
+    for provider, specs in local.identity_pool_providers_raw: provider => {
+      project = var.project_id
+      provider_id = specs.provider
+      pool= specs.pool
+      display_name = lookup(specs, "display_name", specs.pool)
+      description = lookup(specs, "description", null)
+      disabled = lookup(specs, "disabled", false)
+      attribute_mapping = merge(
+        lookup(local.identity_pool_providers_map_trusted, provider, null) == null ? {} : lookup(local.identity_pool_providers_map_trusted[provider], "attributes", {} ),
+        lookup(specs, "attribute_mapping", {})
+      )
+      attribute_condition = lookup(specs, "attribute_condition", null) == null ? try(
+        format(local.identity_pool_providers_map_trusted[provider].condition, specs.owner), null
+      ) : specs.attribute_condition
+      oidc = lookup(specs, "oidc", null) == null ? null : {
+        issuer = lookup(local.identity_pool_providers_map_trusted, provider, null) == null ? specs.oidc.issuer : try(
+          format(local.identity_pool_providers_map_trusted[provider].issuer, specs.owner), local.identity_pool_providers_map_trusted[provider].issuer
+        )
+        allowed_audiences = concat(
+          [for item in try(local.identity_pool_providers_map_trusted[provider].allowed_audience, []) : try(format(item, specs.owner), item)],
+          try(distinct(specs.oidc.allowed_audiences), [])
+        )
+      }
+      aws = lookup(specs, "aws", null) == null ? null : {
+        account_id = specs.aws.account_id
+      }
+    }
+  }
 }
--- Google/resource-manager/sub_modules/workload_identity_federation/main.tf
+++ Google/resource-manager/sub_modules/workload_identity_federation/main.tf
@@ -5,31 +5,31 @@ resource google_iam_workload_identity_pool self {
   workload_identity_pool_id = each.key
   display_name = each.value.display_name == null ?  each.key : each.value.display_name
   description = each.value.description
-  disable = each.value.disable
+  disabled = each.value.disabled
 }
 
-//noinspection HILUnresolvedReference
+
 resource google_iam_workload_identity_pool_provider self {
   for_each = local.identity_pool_providers
-  project = var.project_id
-  workload_identity_pool_id = each.value.pool
-  workload_identity_pool_provider_id = each.value.provider
-  display_name = lookup(each.value, "display_name", each.value.pool)
-  description = lookup(each.value, "description", "")
-  disabled = lookup(each.value, "disabled", false)
-  attribute_mapping = lookup(each.value, "attribute_mapping", {})
-  attribute_condition = lookup(each.value, "attribute_condition", {} )
+  project = each.value.project
+  workload_identity_pool_id = google_iam_workload_identity_pool.self[each.value.pool].workload_identity_pool_id
+  workload_identity_pool_provider_id = each.value.provider_id
+  display_name = each.value.display_name
+  description = each.value.description
+  disabled = each.value.disabled
+  attribute_mapping = each.value.attribute_mapping
+  attribute_condition = each.value.attribute_condition
   dynamic "aws" {
-    for_each = lookup(each.value, "aws", null) == null ? {} : each.value.aws
+    for_each = lookup(each.value, "aws", null) == null ? {} : { aws = each.value.aws}
     content {
       account_id = aws.value.account_id
     }
   }
   dynamic "oidc" {
-    for_each = lookup(each.value, "aws", null) == null ? {} : each.value.aws
+    for_each = lookup(each.value, "oidc", null) == null ? {} : { oidc = each.value.oidc }
     content {
-      issuer_uri = lookup(local.known_issuers, oidc.issuer, oidc.issuer)
-      allowed_audiences = lookup(oidc.value, "allowed_audiences")
+      issuer_uri = oidc.value.issuer
+      allowed_audiences = lookup(oidc.value, "allowed_audiences", []) == [] ? null : oidc.value.allowed_audiences
     }
   }
 }
