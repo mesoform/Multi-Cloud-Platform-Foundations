a53430bb39fc3805bd18de7167a8fa208ddcde9d
Alex Bailey <alex.bailey@mesoform.com>
2023-03-14 09:23:45 +0000

    MPS-50 Update module to fit changes in TIM module

--- Google/iam/workload_identity_federation/main.tf
+++ Google/iam/workload_identity_federation/main.tf
@@ -1,6 +1,10 @@
 module workload_identity_pools {
-  source                 = "github.com/mesoform/terraform-infrastructure-modules//gcp/iam/workload_identity_federation?ref=06fd7e1879a9d968a74f2f278af1232644228fe2"
-  for_each               = local.workload_identity_pools_specs
-  project_id             = var.project_id == null ? local.workload_identity_pools.project_id : var.project_id
-  workload_identity_pool = each.value
+  source                           = "github.com/mesoform/terraform-infrastructure-modules//gcp/iam/workload_identity_federation?ref=061dc8ab2e66f85f84b6f4b445d791d995198fc8"
+  for_each                         = local.workload_identity_pools_specs
+  project_id                       = var.project_id == null ? local.workload_identity_pools.project_id : var.project_id
+  pool_id                          = each.value.pool_id
+  display_name                     = lookup(each.value, "display_name", null)
+  description                      = lookup(each.value, "description", "")
+  disabled                         = lookup(each.value, "disabled" , false)
+  workload_identity_pool_providers = lookup(each.value, "providers", {})
 }
