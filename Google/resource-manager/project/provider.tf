6b8a99ba5126cac91467bb2c2946d16de63ae1eb
danperezsan <dan@mesoform.com>
2023-07-21 11:46:10 +0200

    Bumps google provider version to avoid conflict

--- Google/resource-manager/project/provider.tf
+++ Google/resource-manager/project/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">=3.55.0, <4.0.0"
+      version = ">=3.55.0, <5.0.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-      version = ">=3.55.0, <4.0.0"
+      version = ">=3.55.0, <5.0.0"
     }
   }
 }
